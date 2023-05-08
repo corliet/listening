e0cbf09696a89a4c3b63f5eaf3efa113fda3d5ff
corliet <tai.y271828@gmail.com>
2023-05-08 21:23:52 +0900

    Fix issue that install.ps1 had undefined reference

--- install.ps1
+++ install.ps1
@@ -1,10 +1,15 @@
 function install {
+    $listeningDir = Join-Path -Path $HOME -ChildPath "リスニング"
     $target = Join-Path -Path $listeningDir -ChildPath "listening.html"
-    $shortcutPath = Join-Path -Path $home -ChildPath "Desktop\リスニング.lnk"
+    $shortcutPath = Join-Path -Path $HOME -ChildPath "Desktop\リスニング.lnk"
+    if (Test-Path -Path $shortcutPath) {
+        Remove-Item -Path $shortcutPath
+    }
     $WShell = New-Object -ComObject WScript.Shell
     $shortcut = $WShell.CreateShortcut($shortcutPath)
-    $shortcut.TargetPath = $targetPath
+    $shortcut.TargetPath = [String]$target
     $shortcut.Save()
+    Write-Host "Successfully created shortcut."
 }
 
 install
